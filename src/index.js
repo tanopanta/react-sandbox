5f7531a9748d1a0afde75a33a313586348a6dcad
tanopanta <25007602+tanopanta@users.noreply.github.com>
2021-07-19 09:47:04 +0900

    add spread

--- src/index.js
+++ src/index.js
@@ -69,5 +69,27 @@
 // const [a, b] = hoge;
 // console.log(a, b);
 
-const sayHello = (name = "anonymous") => console.log(`hello ${name}`);
-sayHello();
+// const sayHello = (name = "anonymous") => console.log(`hello ${name}`);
+// sayHello();
+
+// const arr1 = [1, 2];
+// console.log(...arr1);
+
+// const sumFunc = (a, b) => console.log(a + b);
+// sumFunc(...arr1)
+
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1, num2);
+// console.log(arr3);
+
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+console.log(arr6);
+const [...arr7] = arr4;
+console.log(arr7);
+
+const arr8 = [...arr4, ...arr5];
+console.log(arr8);
